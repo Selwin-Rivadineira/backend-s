04a4f8785f33e0a7e989523033221511479cc21a
milton Ayca <miltonaycacolque1@gmail.com>
2025-12-01 16:01:57 -0400

    config: actualizar configuración de entorno

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -9,4 +9,12 @@ export const ENV = {
   MONGO_PASS: process.env.MONGO_PASS,
   MONGO_HOST: process.env.MONGO_HOST,
   MONGO_DB: process.env.MONGO_DB,
+
+  MONGODB_URI: process.env.MONGODB_URI,
+  SMTP_HOST: process.env.SMTP_HOST_NOT,
+  SMTP_PORT: parseInt(process.env.SMTP_PORT_NOT || '587'),
+  SMTP_SECURE: process.env.SMTP_SECURE_NOT === 'true',
+  SMTP_USER: process.env.SMTP_USER_NOT,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD_NOT,
+  SMTP_FROM: process.env.SMTP_FROM_NOT,
 };
