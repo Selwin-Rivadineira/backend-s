7e75a1247f56edfc81a046587e4a2be6238a33d3
Miguel <miguelangelmassigeronimo@gmail.com>
2025-12-02 08:06:05 -0400

    Fix import statement and remove unused route
    Removed non-existent health.routes import and fixed formatting.

--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {  listUsers , getUserById} from "../controllers/user.controller";
+import { listUsers, getUserById } from "../controllers/user.controller";
 import { postDescriptionFixer } from "../../controllers/user.controller";
-//import router from "./health.routes";
+// Se eliminó la importación de health.routes que no existía y causaba error
 const routerUser = Router();
 
 routerUser.get("/users", listUsers);
 routerUser.get("/:id", getUserById); 
 routerUser.post('/:id/description', postDescriptionFixer);
+
 export default routerUser;
